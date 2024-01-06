686e11a989e838fc6e30db45b8cb8a30d32969c9
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2024-01-06 19:11:18 +0530

    Update and rename Merge-sort-tree.txt to Merge-sort-tree.cpp

--- Merge-sort-tree.cpp
+++ Merge-sort-tree.cpp
@@ -73,19 +73,6 @@ vector<int> query(int treeIndex, int Tstart, int Tend, int Qstart, int Qend){
     return comb(left, right);
 }
 
-void euler(vector<int> &tour, int node, vector<vector<int>> & tree, int &index, map<int, pair<int, int>>& startEnd, bool vis[]){
-    tour.emplace_back(node);
-    startEnd[node].first = index;
-    index++;
-    vis[node] = true;
-    for(int child: tree[node])
-    {
-        if(!vis[child])
-            euler(tour, child, tree, index, startEnd, vis);
-    }
-    startEnd[node].second = index-1;
-}
-
 initialize_building_tree(...build parameters) {
     tree.resize(cost.size() * 4, vector<int>(0));
     build(...build parameters);
